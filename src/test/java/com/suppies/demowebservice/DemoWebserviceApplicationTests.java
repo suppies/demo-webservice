235786d4998f3f91241e64d9f759af5a0508a76d
Michael Avila <bobster12707@gmail.com>
2023-10-30 11:58:42 -0400

    inital commit

---
+++ src/test/java/com/suppies/demowebservice/DemoWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suppies.demowebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
